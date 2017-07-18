1731ab83a0305a6c4fd752f117b348d030d20377
barancankatesen <barancankatesen@hotmail.com>
2017-07-18 16:07:37 +0300

    Home Layout ve Admin Layout Düzenlendi Home içindeki sidebar'da kategoriler listelendi

---
+++ BlogLayer/CF/RazorBlogContext.cs
@@ -0,0 +1,20 @@
+﻿using BlogLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BlogLayer.CF
+{
+    public class RazorBlogContext:DbContext
+    {
+        public RazorBlogContext()
+        {
+            Database.Connection.ConnectionString = "Server=.;Database=MyBlog;User Id=sa;Password=123456789?";
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
